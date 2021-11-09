3e3372927153594f172b5e6d3f19c78386064fca
parke2023059 <parke2023059@live.ytech.edu>
2021-11-09 14:24:05 -0500

    Base Server

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require('express');
-app = express()
+const http = require('http');
 
-const port = 8080
-const IP = '127.0.0.1'
+//ip the server uses
+const hostname = '127.0.0.1';
 
-app.listen()
+//port the server uses
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+
+  //sends back status code 200 to give the ay ok
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  //prints ‘Hello World’ to the screen when you access the website
+  res.end('Hello World');
+});
+//creates a listen server
+server.listen(port, hostname, () => {
+  //prints the server is running into the command prompt
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
