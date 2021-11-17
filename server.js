eb6e9f7dc6fd28bdc7d996057286ad876b29a87e
parke2023059 <parke2023059@live.ytech.edu>
2021-11-17 11:25:12 -0500

    update 4

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const readline = require('readline');
 app = express()
 const hostname = '127.0.0.1'
 const port = 1337;
+var fs = require('fs')
+
+
+//const test = {
+  //name:'Parker',
+  //age: 16
+//}
+
+//var saveTest = JSON.stringify(test)
+  //fs.writeFile('test.json', saveTest, 'utf8', function(){
+    //console.log('Wrote To File')
+  //})
+
+
+
 
 
 app.set('view engine', 'ejs');
@@ -32,5 +47,5 @@ app.post('/access', function(req, res) {
 
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server Is Running At: 127.0.0.1:1337`);
 });
