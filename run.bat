8b72a82e8dec6ed17cfd0c8a840f2fa5081eb935
Tsiegmund <Tyler2023411@live.ytech.edu>
2021-11-17 11:24:46 -0500

    Nearly finished working on JSON input

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
 node server.js
+cmd -k
--- server.js
+++ server.js
@@ -1,17 +1,51 @@
 const express = require('express');
 const readline = require('readline');
+const fs = require('fs');
 app = express()
 const hostname = '127.0.0.1'
+app.use(express.urlencoded({ extended: true}));
 const port = 1337;
 
 
 app.set('view engine', 'ejs');
 
+var rawdata = fs.readFileSync(__dirname + '/userinfo.json');
+
 
 app.get('/', function(req, res) { //view and edit scores
+  var rawinfo = fs.readFileSync(__dirname + "/userinfo.json")
+  feed = JSON.parse(rawinfo)
   res.render('index.ejs', { //depending on perms ^
+    username:uName,
+    password: passw
   });
 });
+
+app.post('/', function(req, res) {
+  const user = req.body.Uname
+  const pass = req.body.Pass
+  var login = {
+    uName: user,
+    passw: pass
+  }
+
+  if (login.username && login.password) {
+    var rawinfo = fs.readFileSync('./userinfo')
+    feed = JSON.parse(rawinfo)
+    console.log(feed)
+    comments = feed.comments
+    comments.push(login)
+    var feed = {
+      comments: comments
+    }
+    var feed = JSON.stringify(feed)
+
+    fs.writeFile(__dirname + '/userinfo.json', feed, 'utf8', function() {
+      console.log('wrote to file')
+      console.log(feed)
+    })
+  }
+})
 
 app.get('/access', function(req, res) {
   res.render('access.ejs', {//list of users and their scores, also used for adding and removing students
